fe2e2ed12668506eb294ffeec6bbb225eb3722ab
Joel Wittke <48647033+Xynox@users.noreply.github.com>
2022-10-04 22:57:09 +0200

    Fix exception and add checkmarks to see which operation mode you are in
    Goal of this commit was to fix a bug that occured when the input in the
    input fields was empty (and with that invalid) and to address the problem
    that the current operation was not visible when clicked which was solved by
    putting in checkmarks that are not clickable for the user but get changed
    with each iteration of an operation change
    
    Fixed an exception that occured when the float on input_1 or input_2 was invalid (ll. 151 & 157)
    Added boolean to check if any operations were selected before (l. 52)
    When operation is selected checkmark gets checked and others unchecked (ll. 70-140)

--- unit1.pas
+++ unit1.pas
@@ -12,6 +12,10 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    check_multiply: TCheckBox;
+    check_divide: TCheckBox;
+    check_plus: TCheckBox;
+    check_minus: TCheckBox;
     op_plus: TButton;
     op_minus: TButton;
     op_multiply: TButton;
@@ -22,9 +26,11 @@ type
     Label1: TLabel;
     Label2: TLabel;
     result: TLabel;
+    procedure check_plusChange(Sender: TObject);
     procedure confirmButtonClick(Sender: TObject);
     procedure e_1Change(Sender: TObject);
     procedure e_2Change(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure Label1Click(Sender: TObject);
     procedure Label2Click(Sender: TObject);
     procedure op_divideClick(Sender: TObject);
@@ -43,6 +49,7 @@ var
   op: Integer;
   input_1: real;
   input_2: real;
+  checked: boolean;
 
 implementation
 
@@ -62,23 +69,74 @@ end;
 
 procedure TForm1.op_divideClick(Sender: TObject);
 begin
+  if checked=True then
+
+      if op=1 then
+        check_plus.State:=cbUnchecked;
+
+      if op=2 then
+        check_minus.State:=cbUnchecked;
+
+      if op=3 then
+        check_multiply.State:=cbUnchecked;
+
   op:=4;
-  op_divide.Color:=clRed;
+  check_divide.State:=cbChecked;
+  checked:=True;
 end;
 
 procedure TForm1.op_minusClick(Sender: TObject);
 begin
+  if checked=True then
+
+      if op=1 then
+        check_plus.State:=cbUnchecked;
+
+      if op=4 then
+        check_divide.State:=cbUnchecked;
+
+      if op=3 then
+        check_multiply.State:=cbUnchecked;
+
   op:=2;
+  check_minus.State:=cbChecked;
+  checked:=True
 end;
 
 procedure TForm1.op_multiplyClick(Sender: TObject);
 begin
+  if checked=True then
+
+      if op=1 then
+        check_plus.State:=cbUnchecked;
+
+      if op=2 then
+        check_minus.State:=cbUnchecked;
+
+      if op=4 then
+        check_divide.State:=cbUnchecked;
+
   op:=3;
+  check_multiply.State:=cbChecked;
+  checked:=True
 end;
 
 procedure TForm1.op_plusClick(Sender: TObject);
 begin
+  if checked=True then
+
+      if op=4 then
+        check_divide.State:=cbUnchecked;
+
+      if op=2 then
+        check_minus.State:=cbUnchecked;
+
+      if op=3 then
+        check_multiply.State:=cbUnchecked;
+
   op:=1;
+  check_plus.State:=cbChecked;
+  checked:=True
 end;
 
 procedure TForm1.resultClick(Sender: TObject);
@@ -89,17 +147,22 @@ end;
 procedure TForm1.e_1Change(Sender: TObject);
 //var input_1:real;
 begin
-  input_1:=StrToFloat(e_1.Text);
+  
+  input_1:=StrToFloatDef(e_1.Text, 0);
 end;
 
 procedure TForm1.e_2Change(Sender: TObject);
 //var input_2:real;
 begin
-  input_2:=StrToFloat(e_2.Text);
+  input_2:=StrToFloatDef(e_2.Text, 0);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  checked:=False;
 end;
 
 procedure TForm1.confirmButtonClick(Sender: TObject);
-var res: real;
 begin
 
 
@@ -127,6 +190,11 @@ begin
      begin
        result.Caption:=FloatToStr(input_1/input_2);
      end;
+end;
+
+procedure TForm1.check_plusChange(Sender: Tobject);
+begin
+
 end;
 
 end.
